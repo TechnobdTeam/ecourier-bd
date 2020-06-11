cbec45a06c7085e8c98155471d28db9e84bfb9b5
Nur E Alam Khan <porosh@technobd.com>
2020-06-11 12:00:02 +0600

    refactor v1

--- src/ECourierBD/ECourierBD.php
+++ src/ECourierBD/ECourierBD.php
@@ -1,8 +1,8 @@
 <?php 
-namespace ECourierBD;
+namespace technobd\ecourierbd;
 class ECourierBD
 {
-    public static function ECourierBD()
+    public static function hello()
     {
         return 'Hello World, Composer!';
     }
